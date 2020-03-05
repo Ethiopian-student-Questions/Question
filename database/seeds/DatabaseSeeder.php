5548932d69bf7266c08a80c7cfa61d8a5845c50d
GutemaG <bir13gud17@gmail.com>
2020-03-05 12:23:59 +0300

    no

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,15 +9,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-<<<<<<< HEAD
-    public function run(){
-        factory(App\User::class, 1)->create();
-=======
+
     public function run()
     {
          factory(App\User::class, 1)->create();
          factory(App\Grade::class, 12)->create();
->>>>>>> 2a7e57582ab4ab6d35ea1978c370acdad37d00fb
     }
   
 }
