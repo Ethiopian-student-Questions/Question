da98945663732f3f1473077decf532fe39742476
henokfekade <henokfekade2468@gmail.com>
2020-03-05 11:49:25 +0300

    add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,44 +25,46 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $this->ajustQuestionAnswerExplanation();
         if(Gate::allows('isAdmin')) {
             $users = UserController::index();
             return view('admin.adminHome', compact('users'));
         }
         elseif(Gate::allows('isAdvisor')) {
-            $questions = $this->ajustQuestionAnswerExplanation();
+            $questions = $this->adjustQuestionAnswerExplanation();
             return view('question.index', compact('questions'));
         }
     }
 
-    private function ajustQuestionAnswerExplanation()    
+    private function adjustQuestionAnswerExplanation()    
     {
         $questions = auth()->user()->questions;
         foreach ($questions as $question) {
-            if($question->answer->count() > 0) {
 
                 $answer = $question->answer;
-                $correctAnswer = $answer->correct;
-                $incorrectAnswerFromJson = json_decode($answer->incorrect, true);
-                //select correct answer choise
-                $choise = ['A', 'B', 'C', 'D'];
-                $correctAnswerChoise = rand(0, 3);
-
-                // give choice to all answers
-                $incorrectAnswer = array();
-                $count = 0;
-                for ($i=0; $i < 4; $i++) { 
-                    if($i != $correctAnswerChoise) {
-                        $answer[$i] = $incorrectAnswerFromJson[$count++];
-                    }
-                    else {
-                        $answer[$i] = $correctAnswer;
-                    }
-                }
+                $question['correctAnswer'] = $answer->correct;
+                $question['incorrectAnswer'] = json_decode($answer->incorrect, true);
+                $question['explanation'] = $question->explanation->body;
+                
             }
-            $question['answer'] = $answer;
-            return $question;
-            }
+        return $questions;
     }
 }
+
+// $answer = $question->answer;
+//                 $correctAnswer = $answer->correct;
+//                 $incorrectAnswerFromJson = json_decode($answer->incorrect, true);
+//                 //select correct answer choise
+//                 $choise = ['A', 'B', 'C', 'D'];
+//                 $correctAnswerChoise = rand(0, 3);
+
+//                 // give choice to all answers
+//                 $incorrectAnswer = array();
+//                 $count = 0;
+//                 for ($i=0; $i < 4; $i++) { 
+//                     if($i != $correctAnswerChoise) {
+//                         $answer[$i] = $incorrectAnswerFromJson[$count++];
+//                     }
+//                     else {
+//                         $answer[$i] = $correctAnswer;
+//                     }
+//                 }
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -3,68 +3,48 @@
 @section('content')
       <div class="container">
             @if ($questions->count() > 0)
-                  @if ($questions->count() ==1)
-                        <div class="list-group">
-                              <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                                    
-                                    <div class="d-flex w-100 justify-content-between">
-                                          <h5 class="mb-1">Questions</h5>
-                                    </div>
-                                    <div id="accordion">
-                                          <div class="card">
-                                                <div class="card-header" id="headingThree">
-                                                      <h5 class="mb-0">
-                                                      <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                      {{ $questions->body }}
-                                                      </button>
-                                                      </h5>
-                                                </div>
-                                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                                                      <div class="card-body">
-                                                            <div class="container">
-                                                                  @foreach ($questions->answer as $key => $value)
-                                                            <li>{{$key}} {{$value}}</li>
-                                                                      
-                                                                  @endforeach
-                                                            </div>
-                                                            <div class="btn ml-3 btn-danger">Delete</div>
-                                                      </div>
-                                                </div>
-                                          </div>
-                                    </div>
-                              </a>
-                        </div>
-                  @else
-                        @foreach ($questions as $question )
-                              <div class="list-group">
-                                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                                          
-                                          <div class="d-flex w-100 justify-content-between">
-                                                <h5 class="mb-1">Questions</h5>
-                                          </div>
-                                          <div id="accordion">
-                                                <div class="card">
-                                                      <div class="card-header" id="headingThree">
-                                                            <h5 class="mb-0">
-                                                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                            {{ $question->body }}
-                                                            </button>
-                                                            </h5>
-                                                      </div>
-                                                      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                                                            <div class="card-body">
-                                                                  <div class="container">
-                                                                        <li>{{$question->answer}}</li>
-                                                                  </div>
-                                                                  <div class="btn ml-3 btn-danger">Delete</div>
-                                                            </div>
-                                                      </div>
-                                                </div>
-                                          </div>
-                                    </a>
+                  @foreach ($questions as $question )
+                  <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                              
+                              <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Questions</h5>
                               </div>
-                        @endforeach
-                  @endif
+                              <div id="accordion">
+                                    <div class="card">
+                                          <div class="card-header" id="headingThree">
+                                                <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                {{ $question->body }}
+                                                </button>
+                                                </h5>
+                                          </div>
+                                          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                                <div class="card-body">
+                                                      <div class="container">
+                                                            <p>A. {{$question->correctAnswer}} <span class="ml-3 right badge badge-success">correct answer</span></p>
+                                                            <p>B. {{$question->incorrectAnswer[0]}}</p>
+                                                            <p>C. {{$question->incorrectAnswer[1]}}</p>
+                                                            <p>D. {{$question->incorrectAnswer[2]}}</p>
+                                                      </div>
+                                                </div>
+                                                <div class="card-body">
+                                                      <div class="container mt-2">
+                                                            <p class="text-primary">Explanation</p>
+                                                            <p>{{ $question->explanation}}</p>
+                                                      </div>
+                                                </div>
+                                                <form action="/question/{{ $question->id }}" class="mt-3" method="post">
+                                                      @csrf
+                                                      {{ method_field('DELETE') }}
+                                                      <button type="submit" class="btn btn-danger">delete</button>
+                                                  </form>
+                                          </div>
+                                    </div>
+                              </div>
+                        </a>
+                  </div>
+            @endforeach
             @else
                   <div class="list-group">
                         <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
